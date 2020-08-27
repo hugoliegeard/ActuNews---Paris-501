44f8be989851c70d2481a53b4f654f77dc29c75e
Hugo LIEGEARD <hugo@biyn.media>
2020-08-27 16:37:22 +0200

    Finalisation Upload Image

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -13,6 +13,8 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -48,42 +50,34 @@ class PostController extends AbstractController
         # Affectation du User à l'article
         $post->setUser($user);
 
-        dump($post);
-
         # Création du Formulaire
         $form = $this->createFormBuilder($post)
-
             ->add('title', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Titre de l\'article'
                 ]
             ])
-
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'title',
                 'label' => false
             ])
-
             ->add('content', TextareaType::class, [
                 'label' => false
             ])
-
             ->add('image', FileType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'dropify'
                 ]
             ])
-
             ->add('submit', SubmitType::class, [
                 'label' => 'Publier mon article',
                 'attr' => [
                     'class' => 'btn-block btn-dark'
                 ]
             ])
-
             ->getForm();
 
         # Permet à Symfony de traiter les données reçus.
@@ -91,18 +85,48 @@ class PostController extends AbstractController
 
         # Vérifier si le formulaire est soumis
         # Vérifier si les données sont valides
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             # Génération de l'Alias
             $post->setAlias(
-              $slugger->slug(
-                  $post->getTitle()
-              )
+                $slugger->slug(
+                    $post->getTitle()
+                )
             );
 
-            dd($post);
-            # Enclencher la sauvegarde des données
+            # ---------------------- Upload de l'image
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('image')->getData();
+            $newFilename = $post->getAlias() . '-' . uniqid() . '.' . $imageFile->guessExtension();
 
+            // Move the file to the directory where brochures are stored
+            try {
+                $imageFile->move(
+                    $this->getParameter('posts_directory'),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                # TODO Handle catch exception
+            }
+
+            $post->setImage($newFilename);
+            # ---------------------- Upload de l'image
+
+            # Enclencher la sauvegarde des données
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            # Notification Flash qui se base sur les sessions.
+            $this->addFlash('notice',
+                'Félicitation votre article est en ligne !');
+
+            # Redirection
+            return $this->redirectToRoute('default_article', [
+                'category' => $post->getCategory()->getAlias(),
+                'alias' => $post->getAlias(),
+                'id' => $post->getId()
+            ]);
         }
 
         # Transmission du formulaire à la vue
